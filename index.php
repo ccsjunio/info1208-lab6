dbb404908b6ae8e8fc6ae2d071b6038bdd846408
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2020-02-26 17:38:28 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en-US">
+
+<head>
+    <title>INFO1208 - LAB06 - CARLOS FERRAZ</title>
+    <meta charset="utf8" />
+
+    <script src="./main.js"></script>
+</head>
+
+<body>
+    < /body> < /html>
